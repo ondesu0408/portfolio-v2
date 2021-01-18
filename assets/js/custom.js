23994a0483fa8b400b8dea86d4be76ccf5005a42
Eungyu Park <eungyupark@Eungyus-MacBook-Pro.local>
2021-01-18 15:02:57 -0500

    Updated on JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,15 +2,8 @@
 Nav Menu
 */
 var mobile_icon = document.getElementById("mobile-menu-icon");
-// var nav_menu = document.getElementById("nav-container");
 var nav_ul = document.getElementById("nav-ul-container");
 var w = window.innerWidth;
-
-// console.log(mobile_icon);
-// console.log("mobile_icon.src", mobile_icon.src);
-
-// console.log(nav_menu);
-// console.log("logo", test);
 
 mobile_icon.addEventListener("click", function () {
     if (mobile_icon.src.includes("mobile-menu")) {
@@ -25,38 +18,6 @@ mobile_icon.addEventListener("click", function () {
         nav_ul.style.display = "none";
     }
 });
-
-// if(w<770){
-//     nav_ul.style.display = "none";
-//     mobile_icon.addEventListener("click", function(){
-//         if(mobile_icon.src.includes("mobile-menu")){
-//             mobile_icon.setAttribute("src", "assets/images/close-icon.png");
-//             mobile_icon.setAttribute("alt", "Close Icon");
-//             mobile_icon.style.width = "100%";
-//             nav_ul.style.display = "flex";
-//         }
-//         else{
-//             mobile_icon.setAttribute("src", "assets/images/mobile-menu-icon.png");
-//             mobile_icon.setAttribute("alt", "Mobile Menu Icon");
-//             nav_ul.style.display = "none";
-//         }
-//     });
-// }
-// else{
-//     nav_ul.style.display = "inline";
-// }
-
-
-// Picutre Overlay
-
-// function on(){
-//     console.log("document.getElementById('overlay')", document.getElementsByClassName("overlay"))
-//     document.getElementById("overlay").style.display = "block";
-// }
-
-// function off(){
-//     document.getElementById("overlay").style.display = "none";
-// }
 
 // Go to Top
 // Reference: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
